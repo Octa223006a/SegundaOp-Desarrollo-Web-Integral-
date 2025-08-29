5f38e271338f371d8238825facb7c2967f1965e1
Octa223006a <benjamincarmine789@gmail.com>
2025-08-29 17:10:23 -0600

    playeras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { About } from './about/about';
 import { Contact } from './contact/contact';
+import { Tienda } from './tienda/tienda';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
+  { path: 'tienda', component: Tienda },
   { path: '**', redirectTo: '' } // ruta por defecto
 ];
